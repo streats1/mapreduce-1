5696a0f0ef3325f8b69c903039e95ce20f069164
BIT <BIT@DESKTOP-DOGVD1T>
2017-03-17 15:47:11 +0900

    .: 처리, 대소문자 구분안함 처리

--- src/main/java/com/bit2017/mapreduce/WordCount.java
+++ src/main/java/com/bit2017/mapreduce/WordCount.java
@@ -27,9 +27,9 @@ public class WordCount {
 			
 			String line = value.toString();
 			StringTokenizer tokenizer = 
-					 new StringTokenizer( line, "\r\n\t,|()<> ''" );
+					 new StringTokenizer( line, "\r\n\t,|()<> ''.:" );
 			while( tokenizer.hasMoreTokens() ) {
-				word.set( tokenizer.nextToken() );
+				word.set( tokenizer.nextToken().toLowerCase() );
 				context.write( word, one );
 			}
 		}
