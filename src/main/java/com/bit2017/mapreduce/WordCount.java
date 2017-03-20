b121a5fb1fe557244fa673fba756bc0946dacac9
BIT <BIT@DESKTOP-DOGVD1T>
2017-03-20 14:28:50 +0900

    Mapper 메소드 오버라이드

--- src/main/java/com/bit2017/mapreduce/WordCount.java
+++ src/main/java/com/bit2017/mapreduce/WordCount.java
@@ -3,6 +3,8 @@ package com.bit2017.mapreduce;
 import java.io.IOException;
 import java.util.StringTokenizer;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.LongWritable;
@@ -17,13 +19,23 @@ import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
 
 public class WordCount {
 
+	private static Log log = LogFactory.getLog( WordCount.class );
+	
 	public static class MyMapper extends Mapper<LongWritable, Text, Text, LongWritable> {
 		private Text word = new Text();
 		private static LongWritable one = new LongWritable(1);
 		
 		@Override
+		protected void setup(Mapper<LongWritable, Text, Text, LongWritable>.Context context)
+				throws IOException, InterruptedException {
+			log.info( "------> setup() called" );
+		}
+
+		@Override
 		protected void map(LongWritable key, Text value, Mapper<LongWritable, Text, Text, LongWritable>.Context context)
 				throws IOException, InterruptedException {
+			
+			log.info( "-------> map() called" );
 			
 			String line = value.toString();
 			StringTokenizer tokenizer = 
@@ -33,6 +45,19 @@ public class WordCount {
 				context.write( word, one );
 			}
 		}
+
+		@Override
+		protected void cleanup(Mapper<LongWritable, Text, Text, LongWritable>.Context context)
+				throws IOException, InterruptedException {
+			log.info( "-------> cleanup() called" );
+		}
+
+		//run 은 보통 Override를 하지 않는다.
+//		@Override
+//		public void run(Mapper<LongWritable, Text, Text, LongWritable>.Context context)
+//				throws IOException, InterruptedException {
+//			super.run(context);
+//		}
 	}
 
 	public static class MyReducer extends Reducer<Text, LongWritable, Text, LongWritable> {
