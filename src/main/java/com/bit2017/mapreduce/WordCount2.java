d21e799708fefe25bd739d8515b7362c08ec57d2
yattadog <kickscar@gmail.com>
2017-03-21 05:30:57 +0900

    WordCount2

---
+++ src/main/java/com/bit2017/mapreduce/WordCount2.java
@@ -0,0 +1,88 @@
+package com.bit2017.mapreduce;
+
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.input.KeyValueTextInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
+
+import com.bit2017.mapreduce.io.NumberWritable;
+
+public class WordCount2 {
+	
+	public static class MyMapper extends Mapper<Text, Text, Text, NumberWritable> {
+		private Text word = new Text();
+		private static NumberWritable one = new NumberWritable(1L);
+
+		@Override
+		protected void map(Text key, Text value, Mapper<Text, Text, Text, NumberWritable>.Context context)
+				throws IOException, InterruptedException {
+			String line = value.toString();
+			StringTokenizer tokenizer = 
+					 new StringTokenizer( line, "\r\n\t,|()<> ''.:" );
+			while( tokenizer.hasMoreTokens() ) {
+				word.set( tokenizer.nextToken().toLowerCase() );
+				context.write( word, one );
+			}
+		}
+	}
+
+	public static class MyReducer extends Reducer<Text, NumberWritable, Text, NumberWritable> {
+
+		private NumberWritable sumWritable = new NumberWritable();
+		
+		@Override
+		protected void reduce(Text key, Iterable<NumberWritable> values,
+				Reducer<Text, NumberWritable, Text, NumberWritable>.Context context) throws IOException, InterruptedException {
+			long sum = 0;
+			for( NumberWritable value : values ) {
+				sum += value.get();
+			}
+			
+			sumWritable.set( sum );
+			context.write( key, sumWritable );
+		}
+	}
+	
+	public static void main(String[] args) throws Exception {
+		Configuration conf = new Configuration();
+		Job job = new Job( conf, "WordCount2" );
+		
+		//Job Instance 초기화 작업
+		job.setJarByClass( WordCount.class );
+		
+		//맵퍼 클래스 지정
+		job.setMapperClass( MyMapper.class );
+		//리듀서 클래스 지정
+		job.setReducerClass( MyReducer.class);
+		
+		job.setCombinerClass( MyReducer.class );
+		
+		//출력 키 타입
+		job.setMapOutputKeyClass( Text.class );
+		//출력 밸류 타입
+		job.setMapOutputValueClass( NumberWritable.class );
+		
+		//입력파일 포맷 지정(생략 가능)
+		job.setInputFormatClass( KeyValueTextInputFormat.class );
+		//7. 출력파일 포맷 지정(생략 가능)
+		job.setOutputFormatClass( TextOutputFormat.class );
+		
+		//입력 파일 이름 지정
+		FileInputFormat.addInputPath(job, new Path(args[0]));
+		
+		//출력 디렉토리 지정
+		FileOutputFormat.setOutputPath(job, new Path(args[1]));
+		
+		//실행
+		job.waitForCompletion( true );
+	}
+}
