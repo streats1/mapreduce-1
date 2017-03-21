9ea97b293424c849b84d6da462dcfdafadac846c
BIT <BIT@DESKTOP-DOGVD1T>
2017-03-21 15:34:28 +0900

    WordCount2 작성

--- src/main/java/com/bit2017/mapreduce/WordCount2.java
+++ src/main/java/com/bit2017/mapreduce/WordCount2.java
@@ -5,6 +5,7 @@ import java.util.StringTokenizer;
 
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.Mapper;
@@ -15,15 +16,16 @@ import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
 
 import com.bit2017.mapreduce.io.NumberWritable;
+import com.bit2017.mapreduce.io.StringWritable;
 
 public class WordCount2 {
 	
-	public static class MyMapper extends Mapper<Text, Text, Text, NumberWritable> {
-		private Text word = new Text();
+	public static class MyMapper extends Mapper<Text, Text, StringWritable, NumberWritable> {
+		private StringWritable word = new StringWritable();
 		private static NumberWritable one = new NumberWritable(1L);
 
 		@Override
-		protected void map(Text key, Text value, Mapper<Text, Text, Text, NumberWritable>.Context context)
+		protected void map(Text key, Text value, Mapper<Text, Text, StringWritable, NumberWritable>.Context context)
 				throws IOException, InterruptedException {
 			String line = value.toString();
 			StringTokenizer tokenizer = 
@@ -35,45 +37,50 @@ public class WordCount2 {
 		}
 	}
 
-	public static class MyReducer extends Reducer<Text, NumberWritable, Text, NumberWritable> {
+	public static class MyReducer extends Reducer<StringWritable, NumberWritable, StringWritable, NumberWritable> {
 
 		private NumberWritable sumWritable = new NumberWritable();
 		
 		@Override
-		protected void reduce(Text key, Iterable<NumberWritable> values,
-				Reducer<Text, NumberWritable, Text, NumberWritable>.Context context) throws IOException, InterruptedException {
+		protected void reduce(StringWritable key, Iterable<NumberWritable> values,
+				Reducer<StringWritable, NumberWritable, StringWritable, NumberWritable>.Context context) throws IOException, InterruptedException {
 			long sum = 0;
 			for( NumberWritable value : values ) {
 				sum += value.get();
 			}
 			
 			sumWritable.set( sum );
+
+			context.getCounter( "Word Status", "Count of all Words" ).increment( sum );
+
 			context.write( key, sumWritable );
 		}
 	}
 	
 	public static void main(String[] args) throws Exception {
 		Configuration conf = new Configuration();
-		Job job = new Job( conf, "WordCount2" );
+		Job job = new Job( conf, "WordCount" );
 		
 		//Job Instance 초기화 작업
 		job.setJarByClass( WordCount.class );
 		
 		//맵퍼 클래스 지정
 		job.setMapperClass( MyMapper.class );
+		
 		//리듀서 클래스 지정
 		job.setReducerClass( MyReducer.class);
 		
+		// 컴바이너 세팅
 		job.setCombinerClass( MyReducer.class );
 		
 		//출력 키 타입
-		job.setMapOutputKeyClass( Text.class );
+		job.setMapOutputKeyClass( StringWritable.class );
 		//출력 밸류 타입
 		job.setMapOutputValueClass( NumberWritable.class );
 		
-		//입력파일 포맷 지정(생략 가능)
+		//입력파일 포맷 지정
 		job.setInputFormatClass( KeyValueTextInputFormat.class );
-		//7. 출력파일 포맷 지정(생략 가능)
+		//출력파일 포맷 지정(생략 가능)
 		job.setOutputFormatClass( TextOutputFormat.class );
 		
 		//입력 파일 이름 지정
